dc8e7f4a6e3b1286781ba74c712310627dc1e538
Zayan-Sheikh <160139662+zyanshykh@users.noreply.github.com>
2024-11-21 17:25:00 +0500

    Typescript mini game

---
+++ web.ts
@@ -0,0 +1,16 @@
+import inquirer from "inquirer";
+
+console.log("Guess a number between 1 to 10");
+
+// now variables create 
+let a:number = Math.floor(Math.random()*10)
+
+while(true){
+    let input = await inquirer.prompt({name:"guess",type:"number",message:"Enter a guess number you want "})
+    // Prodive code 
+    let ans:number= input.guess
+    if(a == ans)
+    {console.log("congratulation your guess number is correct ") 
+        break;}
+        else{console.log("Sorry you guess wrong number try again")}
+}
